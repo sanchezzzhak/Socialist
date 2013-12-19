8b1a50a98214f733ff764ae53a399f10b66a50a1
Tutik Alexsandr <sanchezzzhak@ya.ru>
2013-12-19 18:19:21 +0300

    Update Socialist.php

--- src/Tutik/Socialist/Socialist.php
+++ src/Tutik/Socialist/Socialist.php
@@ -1,9 +1,6 @@
 <?php namespace Tutik\Socialist;
 
-
-
 class Socialist {
-
 
     /**
      * Получить класс для указанной соц сети
@@ -24,7 +21,7 @@ class Socialist {
             case 'fb':
                 return new Facebook (
                     [   'appId'  => $config['fb']['app_id'],
-                        'secret' => $config['fb']['app_key']
+                        'secret' => $config['fb']['app_key'],
                     ]);
         }
 
@@ -36,6 +33,13 @@ class Socialist {
     public static function getRegistrationLinks()
     {
         $config =  \Config::get('api/socialist');
+		
+		$facebook = Socialist::factory('fb');
+		$facebook_url = $facebook->getLoginUrl([
+			'scope' => 'read_stream,friends_likes,email,user_birthday',
+			'redirect_uri'=> $config['fb']['redirect_url'] . '?auth=fb' 
+		]);
+
         return [
             'vk' => 'http://api.vk.com/oauth/authorize?client_id=' . $config['vk']['app_id'] .
             '&response_type=code'. 
@@ -45,16 +49,16 @@ class Socialist {
             'mailru' => 'https://connect.mail.ru/oauth/authorize?client_id=' . $config['mailru']['app_id'] .
             '&response_type=code&redirect_uri='. urldecode($config['mailru']['redirect_url'] . '?auth=mailru'),
 
-            'fb'     => 'https://www.facebook.com/dialog/oauth?client_id=' . $config['fb']['app_id'] .
-            '&redirect_uri=' .urldecode( $config['fb']['redirect_url'] . '?auth=fb'),
-
-            'google_plus' => 'https://accounts.google.com/o/oauth2/auth?redirect_uri=' . urldecode( $config['google_plus']['redirect_url'] . '?auth=google_plus') .
+            'fb'     => $facebook_url,
+		
+			'google_plus' => 'https://accounts.google.com/o/oauth2/auth?redirect_uri=' . urldecode( $config['google_plus']['redirect_url'] . '?auth=google_plus') .
                              '&response_type=code&client_id='. $config['google_plus']['app_id'] .'&approval_prompt=force'.
                              '&scope='. urlencode('https://www.googleapis.com/auth/plus.me') .'&access_type=offline',
-
-            'tw' => '/socialist?auth=tw' ,
-
-        ];
+							 
+            'tw' => '/socialist?auth=tw',	
+		
+		];
+		
     }
 
 
